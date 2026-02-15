26d2c2b35c5fa25216cc1f5817770313b8ffa591
IagoMussel <iago.mussel@ezops.cloud>
2026-02-14 23:31:15 -0300

    remove .docsauros config folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/iagomussel/Documents/huntermussel/node_modules/infima/dist/css/default/default.css"),
-  require("/home/iagomussel/Documents/huntermussel/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/iagomussel/Documents/huntermussel/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/iagomussel/Documents/huntermussel/src/index.css"),
-];
